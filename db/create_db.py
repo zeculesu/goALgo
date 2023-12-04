ca7e10c9cf2554395555f18774de660ac87cb82a
Admin <zeculesu@github.com>
2023-12-04 13:19:28 +0300

    fix html and codirovka

---
+++ db/create_db.py
@@ -0,0 +1,27 @@
+import mysql.connector
+
+
+def delete_db():
+    query_delete = """DROP DATABASE users;"""
+    cursor.execute(query_delete)
+
+
+def make_db():
+    query0 = """CREATE DATABASE gg"""
+    query1 = """CREATE TABLE gg.users(
+                id_user INT AUTO_INCREMENT PRIMARY KEY,
+                username TEXT,
+                password TEXT) """
+    cursor.execute(query0)
+    cursor.execute(query1)
+
+
+if __name__ == '__main__':
+    db = mysql.connector.connect(
+        host="localhost",
+        user="Admin",
+        password="Admin",
+    )
+    cursor = db.cursor()
+    delete_db()
+    make_db()
